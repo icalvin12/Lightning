37feb10e5f62039c23490fb81f526047e678f976
unknown <cahuang@697-334-Bywater.sf.k12.internal>
2014-09-05 11:33:19 -0700

    index.html

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,73 @@
+bolt[] steve = new bolt[10];
+bolt[] steve1 = new bolt[10];
 void setup()
 {
   size(300,300);
+  strokeWeight(2);
+  background(0, 0, 0);
 }
 void draw()
 {
-
+	int y = 0;
+	int x = 0;
+	for(int i = 0; i < 10 ; i++)
+	{
+		steve[i] = new bolt(0,y,0,y);
+		steve[i].show();
+		y = y + 30;
+	}
+	for(int i = 0; i < 10 ; i++)
+	{
+		steve1[i] = new bolt(x,0,x,0);
+		steve1[i].showVertical();
+		x = x + 30;
+	}
 }
+
 void mousePressed()
 {
-
+	background(0,0,0);
 }
 
+class bolt
+{
+	int startX, startY, endX, endY;
+	bolt(int startX1,int startY1,int endX1,int endY1)
+	{
+		startX = startX1;
+		startY = startY1;
+		endX = endX1;
+		endY = endY1;
+	}
+	void show()
+	{
+		stroke(int(random(0, 255)),int(random(0, 255)),int(random(0, 255)));
+		while(endX < 300)
+		{
+			endX = startX + (int)(Math.random() * 10);
+			endY = startY + (int)((Math.random() * 20)-10);
+			line(startX, startY, endX, endY);
+			startX = endX;
+			startY = endY;
+		}
+	}
+	void showVertical()
+	{
+		stroke(int(random(0, 255)),int(random(0, 255)),int(random(0, 255)));
+		while(endY < 300)
+		{
+			endX = startX + (int)((Math.random() * 20)-10);
+			endY = startY + (int)(Math.random() * 10);
+			line(startX, startY, endX, endY);
+			startX = endX;
+			startY = endY;
+		}
+	}	
+	void reset()
+	{
+		startX = 0;
+		startY = 150;
+		endX = 0;
+		endY = 150;
+	}
+}
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,21 +14,79 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+bolt[] steve = new bolt[10];
+bolt[] steve1 = new bolt[10];
 public void setup()
 {
   size(300,300);
-  noLoop();
+  strokeWeight(2);
+  background(0, 0, 0);
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
-}
-public void mousePressed()
-{
-  redraw();
+	int y = 0;
+	int x = 0;
+	for(int i = 0; i < 10 ; i++)
+	{
+		steve[i] = new bolt(0,y,0,y);
+		steve[i].show();
+		y = y + 30;
+	}
+	for(int i = 0; i < 10 ; i++)
+	{
+		steve1[i] = new bolt(x,0,x,0);
+		steve1[i].showVertical();
+		x = x + 30;
+	}
 }
 
+public void mousePressed()
+{
+	background(0,0,0);
+}
+
+class bolt
+{
+	int startX, startY, endX, endY;
+	bolt(int startX1,int startY1,int endX1,int endY1)
+	{
+		startX = startX1;
+		startY = startY1;
+		endX = endX1;
+		endY = endY1;
+	}
+	public void show()
+	{
+		stroke(PApplet.parseInt(random(0, 255)),PApplet.parseInt(random(0, 255)),PApplet.parseInt(random(0, 255)));
+		while(endX < 300)
+		{
+			endX = startX + (int)(Math.random() * 10);
+			endY = startY + (int)((Math.random() * 20)-10);
+			line(startX, startY, endX, endY);
+			startX = endX;
+			startY = endY;
+		}
+	}
+	public void showVertical()
+	{
+		stroke(PApplet.parseInt(random(0, 255)),PApplet.parseInt(random(0, 255)),PApplet.parseInt(random(0, 255)));
+		while(endY < 300)
+		{
+			endX = startX + (int)((Math.random() * 20)-10);
+			endY = startY + (int)(Math.random() * 10);
+			line(startX, startY, endX, endY);
+			startX = endX;
+			startY = endY;
+		}
+	}	
+	public void reset()
+	{
+		startX = 0;
+		startY = 150;
+		endX = 0;
+		endY = 150;
+	}
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
     if (passedArgs != null) {
